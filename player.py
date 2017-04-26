e366f5d091fbf61194a0a0cc312ce2a6ac066639
Bri N <poko@poko-PC.(none)>
2017-04-26 14:34:07 -0400

    added code that allows us to ask the user for the player name

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player(object):
         self.name = name
         self.good_points = 0
         self.bad_points = 0
-        
+
     def __str__(self):
         # TO DO: Fix the formatting
         print_player = ""
@@ -14,7 +14,7 @@ class Player(object):
         print_player += "Good Points:\t" + str(self.good_points) + "\n"
         print_player += "Bad Points:\t" + str(self.bad_points) + "\n"
         return print_player
-    
+
     def add_good_points(self):
         self.good_points += 5
         return self.good_points
@@ -29,3 +29,14 @@ class Player(object):
         # Returns the TOTAL amount of points to trigger the story line
         return player.TOTAL
 
+
+# The code below will be moved into our main file when we are ready
+# I suggest asking the user for their name before the story rolls
+player_name = str(input("Hi. What would you like your name to be?: "))
+new_player = Player(player_name)
+
+# To call the name, use (don't forget to add the module in the front):
+print(new_player.name)
+
+# This makes sure the name prints currently into the class
+print(new_player)
