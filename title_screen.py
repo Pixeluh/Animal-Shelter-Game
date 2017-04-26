9ca28e3d2f1da5dc9a5c547527e37bfdb51e6d9f
Bri N <poko@poko-PC.(none)>
2017-04-25 21:16:56 -0400

    added the title screen module

---
+++ title_screen.py
@@ -0,0 +1,27 @@
+# Print out the title / menu
+import sys
+
+game_title = """
+┌┬┐┌─┐┌┐┌┌┬┐  ┌─┐┌─┐┬  ┬
+ │││ ││││ │   ├┤ ├─┤│  │
+─┴┘└─┘┘└┘ ┴   └  ┴ ┴┴─┘┴─┘
+"""
+
+game_title_menu = """
+Select an option:
+0) Quit
+1) Start
+"""
+
+print(game_title)
+
+user_response = ""
+
+while user_response not in ("0", "1"):
+    user_response = str(input(game_title_menu))
+    if user_response == "0":
+        sys.exit()
+    elif user_response == "1":
+        # run the main code here to start the game
+        # the print() is used as a filler here
+        print("Yay you started the game!")
