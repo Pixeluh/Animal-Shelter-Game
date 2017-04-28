6e3df85ddeb0f1c5677f38d14805352d9d0a35aa
Bri N <poko@poko-PC.(none)>
2017-04-28 11:29:46 -0400

    the damn map now moves!!!! YEE HAW

--- map_v2.py
+++ map_v2.py
@@ -24,10 +24,9 @@ def print_the_map(the_map):
         for each_character in each_row:
             print(each_character, end="")
 
-the_map[0][0] = "_X_"
+#the_map[0][0] = "_X_"
 row = 0
 character = 0
-player_location = the_map[row][character]
 
 print_the_map(the_map)
 
@@ -36,31 +35,48 @@ print()
 def move_up():
     """ HIHIII """
     player_location = the_map[row-1][character]
-    print(player_location)
+    print("\n", "Spot: \t", player_location, "\n")
+    the_map[row][character] = "___"
+    the_map[row-1][character] = "_X_"
     print_the_map(the_map)
 def move_down():
     player_location = the_map[row+1][character]
-    print(player_location)
+    print("\n", "Spot: \t", player_location, "\n")
+    the_map[row][character] = "___"
+    the_map[row+1][character] = "_X_"
     print_the_map(the_map)
 def move_left():
     """ HIHIII """
     player_location = the_map[row][character-1]
-    print(player_location)
+    print("\n", "Spot: \t", player_location, "\n")
+    the_map[row][character] = "___"
+    the_map[row][character-1] = "_X_"
     print_the_map(the_map)
 def move_right():
     """ HIHIII """
     player_location = the_map[row][character-1]
-    print(player_location)
+    print("\n", "Spot: \t", player_location, "\n")
+    the_map[row][character] = "___"
+    the_map[row][character+1] = "_X_"
     print_the_map(the_map)
 
 while(True):
     key = ord(getch())
     if key == 119:
         move_up()
+        row -= 1
     elif key == 97:
         move_left()
+        character -= 1
     elif key == 115:
         move_down()
+        row += 1
     elif key == 100:
         move_right()
+        character += 1
 
+# TO DO:
+# Replace the characters as we move
+# Set up limitations for movement. (If we move out of the boundry the game crashes)
+# Add a legend below the map
+# Add obstacle/cages to the map
